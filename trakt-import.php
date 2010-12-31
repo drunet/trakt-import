520c5cfbb377bdf98287ba3e59d0f51ecf0ec86c
rectifyer <rectifyer@gmail.com>
2010-12-31 15:40:07 -0800

    api key, smaller batch, more debug message

--- trakt-import.php
+++ trakt-import.php
@@ -4,13 +4,14 @@ define('XBMC_USERNAME', 'username');
 define('XBMC_PASSWORD', 'password');
 define('XBMC_IP', '192.168.0.12');
 define('XBMC_PORT', '8080');
+define('TRAKT_APIKEY', 'key');
 define('TRAKT_USERNAME', 'username');
 define('TRAKT_PASSWORD', 'password');
 
 ########## leave this alone
 
 error_reporting(E_ALL);
-define('BATCH_SIZE', 20);
+define('BATCH_SIZE', 15);
 
 //get movies
 $ch = curl_init();
@@ -23,6 +24,10 @@ preg_match_all('/<field>([^<]*)<\/field><field>([^<]*)<\/field><field>([^<]*)<\/
 
 $movies_library = array();
 $movies_seen = array();
+$missing_title = array();
+$missing_year = array();
+$missing_imdb = array();
+
 for($i = 0; $i < sizeof($movies_matches[0]); $i++) {
 	$a = array(
 		'imdb_id' => $movies_matches[1][$i],
@@ -37,6 +42,10 @@ for($i = 0; $i < sizeof($movies_matches[0]); $i++) {
 	else {
 		$movies_library[] = $a;
 	}
+	
+	if(empty($a['imdb_id'])) $missing_imdb[] = $a;
+	if(empty($a['title'])) $missing_title[] = $a;
+	if(empty($a['year'])) $missing_year[] = $a;
 }
 
 //add seen
@@ -45,7 +54,7 @@ $offset = 0;
 while($offset < sizeof($movies_seen)) {
 	$ch = curl_init();
 	curl_setopt_array($ch, array(
-		CURLOPT_URL => 'http://api.trakt.tv/movie/seen',
+		CURLOPT_URL => 'http://api.trakt.tv/movie/seen/' . TRAKT_APIKEY,
 		CURLOPT_POSTFIELDS => json_encode(array(
 			'username' => TRAKT_USERNAME,
 			'password' => sha1(TRAKT_PASSWORD),
@@ -65,7 +74,7 @@ $offset = 0;
 while($offset < sizeof($movies_library)) {
 	$ch = curl_init();
 	curl_setopt_array($ch, array(
-		CURLOPT_URL => 'http://api.trakt.tv/movie/library',
+		CURLOPT_URL => 'http://api.trakt.tv/movie/library/' . TRAKT_APIKEY,
 		CURLOPT_POSTFIELDS => json_encode(array(
 			'username' => TRAKT_USERNAME,
 			'password' => sha1(TRAKT_PASSWORD),
@@ -78,4 +87,19 @@ while($offset < sizeof($movies_library)) {
 	echo "\t" . $offset . '-' . ($offset + BATCH_SIZE) . ' ' . curl_exec($ch) . "\n";
 	$offset += BATCH_SIZE;
 }
+
+//debug
+echo "\nSummary ==========\n\nMovies missing titles: " . sizeof($missing_title) . "\n";
+foreach($missing_title as $movie) {
+	echo "\t" . $movie['title'] . "\n";
+}
+echo "\nMovies missing years: " . sizeof($missing_year) . "\n";
+foreach($missing_year as $movie) {
+	echo "\t" . $movie['title'] . "\n";
+}
+echo "\nMovies missing imdb ids: " . sizeof($missing_imdb) . "\n";
+foreach($missing_imdb as $movie) {
+	echo "\t\t".$movie['title']."\n";
+}
+echo "\n===============\n";
 ?>
